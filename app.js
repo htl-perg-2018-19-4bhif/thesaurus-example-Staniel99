2936bea4ef44cb607e58e6f1cb6cbf2e30fa8f12
Stefan Daniel <stefan.daniel99@gmx.at>
2018-11-21 20:16:10 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+if (process.argv.length <= 2) {
+    console.error("Please specify words");
+}
+else {
+    var found = false;
+    var input = [];
+    for (var i = 2; i < process.argv.length; i++) {
+        //searching for the given words and printing out the synonyms
+    }
+    if (found == false) {
+        console.log("No matches found");
+    }
+}
---
+++ app.ts
@@ -0,0 +1,14 @@
+if(process.argv.length<=2){
+    console.error("Please specify words");
+}else{
+    let found:Boolean=false;
+    let input:String[]=[];
+
+    for(let i=2; i<process.argv.length; i++){
+        //searching for the given words and printing out the synonyms
+        
+    }
+    if(found==false){
+        console.log("No matches found");
+    }
+}
