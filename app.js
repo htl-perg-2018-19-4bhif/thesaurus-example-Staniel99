e111c53009d629d4d9c621c30e918f431bffbd55
Stefan Daniel <stefan.daniel99@gmx.at>
2018-11-22 00:28:58 +0100

    logic added

--- app.js
+++ app.js
@@ -2,12 +2,34 @@ if (process.argv.length <= 2) {
     console.error("Please specify words");
 }
 else {
-    var found = false;
-    var input = [];
-    for (var i = 2; i < process.argv.length; i++) {
-        //searching for the given words and printing out the synonyms
+    var found_1 = false;
+    var founded_1 = [];
+    for (var i = 0; i < process.argv.length; i++) {
+        founded_1[i] = false;
     }
-    if (found == false) {
-        console.log("No matches found");
-    }
+    var lineReader = require("readline").createInterface({
+        input: require("fs").createReadStream("openthesaurus.txt")
+    });
+    console.log("Matches:");
+    lineReader.on("line", function (line) {
+        var currline = line.split(";");
+        for (var i = 2; i < process.argv.length; i++) {
+            for (var j = 0; j < currline.length; j++) {
+                var currString = currline[j];
+                if (process.argv[i] == currString || currString.indexOf(process.argv[i]) != -1) {
+                    if (founded_1[i] == false) {
+                        console.log(process.argv[i] + ":");
+                        founded_1[i] = true;
+                    }
+                    console.log("\t" + currString);
+                    found_1 = true;
+                }
+            }
+        }
+    });
+    lineReader.on("close", function (line) {
+        if (found_1 == false) {
+            console.log("No matches found!");
+        }
+    });
 }
--- app.ts
+++ app.ts
@@ -2,13 +2,37 @@ if(process.argv.length<=2){
     console.error("Please specify words");
 }else{
     let found:Boolean=false;
-    let input:String[]=[];
+    let founded:Boolean[]=[];
 
-    for(let i=2; i<process.argv.length; i++){
-        //searching for the given words and printing out the synonyms
-        
+    for(let i=0; i<process.argv.length; i++){
+        founded[i]=false;
     }
-    if(found==false){
-        console.log("No matches found");
-    }
+
+    let lineReader = require("readline").createInterface({
+        input: require("fs").createReadStream("openthesaurus.txt")    
+    });
+
+    console.log("Matches:");
+    lineReader.on("line", function (line) {
+        let currline: string[] = line.split(";")
+        for (let i=2; i<process.argv.length; i++) {
+            for (let j=0; j<currline.length; j++) {
+                let currString: string = currline[j];
+                if (process.argv[i]==currString || currString.indexOf(process.argv[i])!=-1){
+                    if(founded[i]==false){
+                        console.log(process.argv[i]+":");
+                        founded[i]=true;
+                    }
+                    console.log("\t"+currString);
+                    found=true;
+                }
+            }
+        }
+    });
+
+    lineReader.on("close", function (line){
+        if (found==false){
+            console.log("No matches found!");
+        }
+    });
 }
